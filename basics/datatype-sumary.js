4081b0778d8421f51025d58fbf9f2b483ed0a641
Rajrkd7 <rajrkd77@gmail.com>
2025-11-08 17:28:35 +0000

    made function

---
+++ basics/datatype-sumary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+//call by value
+
+//7 types : String,Number,Boolean,null,undefined,symbol,BigInt
+
+//Reference (Non primitive)
+
+// Array,objects,functions
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEMail;
+
+const id = Symbol('123')
+const anotherId = symbol('123')
+
+console.log(id === anotherId);
+
+const bigNUmber = 34565434343434n
+
+
+const heros = ["shaktiman","naagraj","doga"]
+let myobj ={
+
+    name:"Raj",
+    age:22
+}
+
+
+const myfunction = function(){
+    console.log("Hello world");
+}
+
+
+
+
+
---
+++ basics/functions.js
@@ -0,0 +1,10 @@
+function addtwoNumbers(number1,number2){ // defined two parameter 
+
+
+    return number1 + number2
+    
+
+}
+
+const result = addtwoNumbers(5,9); // passing argument
+console.log("result:"+ result)
