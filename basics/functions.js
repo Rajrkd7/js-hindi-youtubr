f5f0fbe7fb8f33bc99a8bbe18f12c259cb95eefc
Rajrkd7 <rajrkd77@gmail.com>
2025-11-09 05:22:04 +0000

    made arrow fun

--- basics/functions.js
+++ basics/functions.js
@@ -9,8 +9,23 @@
 // const result = addtwoNumbers(5,9); // passing argument
 // console.log("result:"+ result)
 
-function loginusermessage(username="sam"){ //add default value ,if we give values it just overwrites
-    return `${username} just logged in` 
-}
 
-console.log(loginusermessage("raj"))
+
+// function loginusermessage(username="sam"){ //add default value ,if we give values it just overwrites
+//     return `${username} just logged in` 
+// }
+
+// console.log(loginusermessage("raj"))
+
+// arrow function 
+
+    //     const addTwo = (nuim1,num2) => {
+    //         return num1 + num2
+    //     }
+
+        console.log(addTwo (5,4))
+
+       const addTwo = (num1,num2) => (num1+num2)
+
+       console.log(addTwo(5,9));
+       
